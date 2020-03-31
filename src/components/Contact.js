d13dc9a85bd5936e8728cbba393b2f1c11f9cfdb
thomas <thomas.chalanson@sfr.fr>
2020-03-31 17:28:47 +0200

    add components in src

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+const Contact = {
+    image :"https://randomuser.me/api/portraits/men/34.jpg";
+    status: status-online;
+};
+  
+const User = props => (
+    <div>
+        <img src={props.image} />
+    </div>
+);
+
