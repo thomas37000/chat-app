2541c4667609c514da1c48807e73640dce0cefe2
thomas <thomas.chalanson@sfr.fr>
2020-04-01 13:57:38 +0200

    exo finish

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './Components.css'
+
+const name = "PierrePaulJack"
+const avatar = "https://randomuser.me/api/portraits/men/34.jpg"
+const status = true
+
+/*
+function Contact() {
+    //let status = props.status-online ? 'status - Online' : "status - Offline";
+    
+}
+*/
 
 
-const Contact = {
-    image :"https://randomuser.me/api/portraits/men/34.jpg";
-    status: status-online;
-};
-  
-const User = props => (
-    <div>
-        <img src={props.image} />
-    </div>
-);
+class Contact extends Component {
+    render() {
+        return <div className="Contact">
+        <img className="avatar" src={avatar} />
+        
+        <div>
+          <h4 className="name">{name}</h4>
+          <p className="status">
+              <button className="status-online"></button>
+              {status ? "Online" : "Offline"}
+         </p>
+        </div>
+        
+      </div> 
+    }   
+}
 
+
+export default Contact
